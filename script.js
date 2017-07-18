8a861b730b4d4efb1734415ea2cd9f2d91fac0a0
danielhowlett42 <danielhowlett@hotmail.co.uk>
2017-07-18 11:25:05 +0100

    second test

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
-  $('button').mouseenter(function(){
-      $(this).fadeTo("fast", 0.6);//
-        $(this).click(function(){
-          window.location.href = "firstpage.html";
-        });
-  });
-  $('button').mouseleave(function(){
-      $('button').fadeTo("fast", 1);
-  });
 
+$(document).ready(function(){
+  // var menu = $('#stickyhome').offset().top;
+  // $(window).scroll(function(){
+  //   var ypos = $(this).scrollTop();
+  //   if (ypos >= menu){$('#stickyhome').addClass('fixed');}
+  //   else{$('#stickyhome').removeClass('fixed');}
+  //   $('#stickyhome').width($('#stickyhome').parents().width());
+  // });
+  $('#stickyhome').click(function(){window.location.href="firstpage.html";});
+
+
+
+
+
+  $(this).click(function(){
+    window.location.href = "firstpage.html";
+  });
+  $('img').click(function(){
+    window.location.href = "";
+  });
 });
